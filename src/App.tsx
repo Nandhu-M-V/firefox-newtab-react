4d44891171430594feb0b8001bb1cbaf3e2f8ce2
Nandhu-M-V <nandhumv@netstratum.com>
2026-02-05 18:58:25 +0530

    Basic UI Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import { useState } from 'react';
-import './App.css';
 import bgimage from './assets/images/bg1.jpg';
 
 function App() {
   return (
-    <>
-      <main className="w-full h-full bg-[url({})] "></main>
-    </>
+    <div id="fullcontainer">
+      <div
+        className="w-full h-screen bg-cover bg-center flex justify-center "
+        style={{ backgroundImage: `url(${bgimage})` }}
+      >
+        <main className=" w-full max-w-7xl m-10 h-full flex-col items-center bg-gray-700 ">
+          <div id="logo-firefox" className=" text-white flex gap-7">
+            <img src="" alt="Firefox logo" />
+            <span>Firefox</span>
+          </div>
+
+          <input
+            className="bg-amber-50 h-12 w-xl rounded-md"
+            type="search"
+            placeholder="search with Google or enter address "
+          />
+
+          <div id="shortcutscontainer" className="flex gap-10  ">
+            <div className="bg-gray-600 w-20 h-20 rounded-md">
+              <img src="" alt="" />
+            </div>
+            <div className="bg-gray-600 w-20 h-20 rounded-md">
+              <img src="" alt="" />
+            </div>
+            <div className="bg-gray-600 w-20 h-20 rounded-md">
+              <img src="" alt="" />
+            </div>
+            <div className="bg-gray-600 w-20 h-20 rounded-md">
+              <img src="" alt="" />
+            </div>
+            <div className="bg-gray-600 w-20 h-20 rounded-md">
+              <img src="" alt="" />
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
 
