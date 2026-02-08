bbc39092c890d477b2b7bca982b9ff6ce004f14b
Nandhu-M-V <nandhumv@netstratum.com>
2026-02-08 23:21:23 +0530

    Chore: Add Shadcn Components Switch, Dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import pen from './assets/images/editpen.png';
 import googleLogo from './assets/images/search.png';
 import './App.css';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 interface News {
@@ -20,27 +20,60 @@ import { Sheet, SheetTrigger, SheetContent } from '@/components/ui/sheet';
 function App() {
   const [news, setnews] = useState<News[]>([]);
   const [scrolled, setscrolled] = useState<boolean>(false);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  //   const domain = getDomain(userInput);
+
+  //   domain for shortcuts
+
+  //     function getDomain(url: string) {
+  //       try {
+  //         return new URL(url).hostname;
+  //       } catch {
+  //         return url;
+  //       }
+  //     }
+
+  //   checking scroll
 
   useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines',
-          {
-            params: {
-              country: 'us',
-              apiKey: import.meta.env.VITE_NEWS_API_KEY,
-            },
-          }
-        );
-        setnews(response.data.articles);
-      } catch (error) {
-        console.log(error);
+    const el = scrollRef.current;
+
+    const handleScroll = () => {
+      if (el && el.scrollTop > 0) {
+        setscrolled(true);
+      } else {
+        setscrolled(false);
       }
     };
-    fetchNews();
+
+    el?.addEventListener('scroll', handleScroll);
+
+    return () => el?.removeEventListener('scroll', handleScroll);
   }, []);
 
+  //    api fetching
+  //   useEffect(() => {
+  //     const fetchNews = async () => {
+  //       try {
+  //         const response = await axios.get(
+  //           'https://newsapi.org/v2/top-headlines',
+  //           {
+  //             params: {
+  //               country: 'us',
+  //               apiKey: import.meta.env.VITE_NEWS_API_KEY,
+  //             },
+  //           }
+  //         );
+  //         setnews(response.data.articles);
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     };
+  //     fetchNews();
+  //   }, []);
+
+  //   printing api
   useEffect(() => {
     console.log(news);
   }, [news]);
@@ -50,21 +83,28 @@ function App() {
   return (
     <div id="fullcontainer">
       <div
+        ref={scrollRef}
         className="w-full h-full overflow-scroll bg-cover bg-center fixed flex justify-center p-10"
         style={{ backgroundImage: `url(${bgimage})` }}
       >
         <main className=" w-full max-w-7xl h-full flex flex-col items-center ">
           <div
             id="logo-firefox"
-            className=" text-white flex items-center gap-3 lg:absolute lg:left-12"
+            className={` text-white flex items-center gap-3 [@media(min-width:1200px)]:absolute  lg:left-12 ${scrolled ? 'hidden' : 'block'}`}
           >
             <img src={firefoxLogo} alt="Firefox logo" className="w-14 h-14" />
             <span className="text-3xl font-bold">Firefox</span>
           </div>
+
+          {/* search bar */}
           {/* https://www.google.com/search?client=firefox-b-d&q=hello+world */}
           <div
+            className={`fixed w-screen h-25 top-0 bg-[#42414D]/70 border-b border-b-gray-500 ${scrolled ? 'block' : 'hidden'}`}
+          ></div>
+          <div
             id="search-holder"
-            className="w-full max-w-180 mt-4 flex items-center bg-[#42414D] rounded-md px-3"
+            className={`w-full max-w-180 mt-4 flex items-center bg-[#42414D] rounded-md px-3
+            ${scrolled ? 'sticky -top-3.5 z-50' : ''}`}
           >
             <img src={googleLogo} alt="Google" className="w-5 h-5 mr-3" />
 
@@ -106,6 +146,9 @@ function App() {
               {/* <img src="" alt="" /> */}
             </div>
           </div>
+
+          {/* news section */}
+
           <section id="articlesection" className="">
             <h3 className="text-white font-bold">Thought-provoking stories</h3>
             <div
@@ -117,7 +160,7 @@ function App() {
                   i.urlToImage && (
                     <div
                       key={index}
-                      className=" w-75 h-70 flex flex-col items-center bg-[#42414D]/60 pb-4 rounded-3xl"
+                      className=" w-75 h-70 flex flex-col overflow-hidden items-center bg-[#42414D]/60 pb-4 rounded-3xl"
                     >
                       <img
                         src={i.urlToImage}
@@ -127,13 +170,15 @@ function App() {
                       <p className="w-60 text-[14px] font-semibold text-white">
                         {i.title}
                       </p>
-                      <p className="relative right-18 font-bold text-sm">
+                      <p className="relative w-40 right-14 top-3 text-white font-extralight text-sm">
                         {i.author}
                       </p>
                     </div>
                   )
               )}
             </div>
+
+            {/* sidebar */}
           </section>
           <Sheet>
             <SheetTrigger>
