551f7de180a56338f01494ed6b4c03b927d464f1
Nandhu-M-V <nandhumv@netstratum.com>
2026-02-08 19:53:40 +0530

    Style(UI): fix UI of search bar, button colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import bgimage from './assets/images/bg1.jpg';
 import firefoxLogo from './assets/images/FF.png';
 import pen from './assets/images/editpen.png';
+import googleLogo from './assets/images/search.png';
 import './App.css';
 
 import { useEffect, useState } from 'react';
@@ -11,12 +12,14 @@ interface News {
   description?: string;
   url: string;
   urlToImage?: string;
+  author?: string;
 }
 
 import { Sheet, SheetTrigger, SheetContent } from '@/components/ui/sheet';
 
 function App() {
   const [news, setnews] = useState<News[]>([]);
+  const [scrolled, setscrolled] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchNews = async () => {
@@ -58,117 +61,74 @@ function App() {
             <img src={firefoxLogo} alt="Firefox logo" className="w-14 h-14" />
             <span className="text-3xl font-bold">Firefox</span>
           </div>
+          {/* https://www.google.com/search?client=firefox-b-d&q=hello+world */}
+          <div
+            id="search-holder"
+            className="w-full max-w-180 mt-4 flex items-center bg-[#42414D] rounded-md px-3"
+          >
+            <img src={googleLogo} alt="Google" className="w-5 h-5 mr-3" />
 
-          <div id="search-holder" className=" w-full max-w-150 mt-4">
-            <button>{/* <img src="" alt="" /> */} </button>
             <input
-              className="bg-gray-600 text-white h-13 w-full max-w-150 rounded-md"
+              className="bg-transparent text-white h-13 w-full outline-none"
               type="search"
-              placeholder="search with Google or enter address "
+              placeholder="Search with Google or enter address"
             />
           </div>
-
           <div
             id="shortcutscontainer"
             className="grid lg:grid-cols-8 md:grid-cols-6 sm:grid-cols-3 grid-cols-3 gap-14 mb-10 mt-10 "
           >
+            <div className="bg-[#42414D] opacity-80 text-white w-16 h-16 rounded-2xl">
+              {/* <img src="" alt="" /> */}
+            </div>
             <div className="bg-gray-600 opacity-80 text-white w-16 h-16 rounded-2xl">
               {/* <img src="" alt="" /> */}
             </div>
-            <div className="bg-gray-600 w-16 h-16 text-white rounded-2xl">
+            <div className="bg-gray-600 opacity-80 text-white w-16 h-16 rounded-2xl">
               {/* <img src="" alt="" /> */}
             </div>
-            <div className="bg-gray-600 w-16 h-16 text-white rounded-2xl">
+            <div className="bg-gray-600 opacity-80 text-white w-16 h-16 rounded-2xl">
               {/* <img src="" alt="" /> */}
             </div>
-            <div className="bg-gray-600 w-16 h-16 text-white rounded-2xl">
+            <div className="bg-gray-600 opacity-80 text-white w-16 h-16 rounded-2xl">
               {/* <img src="" alt="" /> */}
             </div>
-            <div className="bg-gray-600 w-16 h-16 text-white rounded-2xl">
-              {/* <img src="" alt="" /> */}
-            </div>
-            <div className="bg-gray-600 w-16 h-16 text-white rounded-2xl">
-              {/* <img src="" alt="" /> */}
-            </div>
-            <div className="bg-gray-600 w-16 h-16 text-white rounded-2xl">
-              {/* <img src="" alt="" /> */}
-            </div>
-            <div className="bg-gray-600 w-16 h-16 text-white rounded-2xl">
+            <div className="bg-gray-600 opacity-80 text-white w-16 h-16 rounded-2xl">
               {/* <img src="" alt="" /> */}
             </div>
           </div>
-
           <section id="articlesection" className="">
             <h3 className="text-white font-bold">Thought-provoking stories</h3>
             <div
               id="article-containers"
               className="grid [@media(min-width:1400px)]:grid-cols-4 [@media(min-width:1050px)]:grid-cols-3 sm:grid-cols-2 gap-6 mt-7 "
             >
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
-              <div className=" w-75 h-65 flex flex-col items-center bg-gray-600/60 pb-4 rounded-3xl">
-                <img src={bgimage} className="w-75 h-45 rounded-t-3xl" alt="" />
-                <p className="w-60 text-white">Random text gooooooooooooooo</p>
-                <p>label</p>
-              </div>
+              {news.map(
+                (i, index) =>
+                  i.urlToImage && (
+                    <div
+                      key={index}
+                      className=" w-75 h-70 flex flex-col items-center bg-[#42414D]/60 pb-4 rounded-3xl"
+                    >
+                      <img
+                        src={i.urlToImage}
+                        className="w-75 h-45 rounded-t-3xl"
+                        alt=""
+                      />
+                      <p className="w-60 text-[14px] font-semibold text-white">
+                        {i.title}
+                      </p>
+                      <p className="relative right-18 font-bold text-sm">
+                        {i.author}
+                      </p>
+                    </div>
+                  )
+              )}
             </div>
           </section>
           <Sheet>
             <SheetTrigger>
-              <a className=" p-2 fixed z-10 right-10 bottom-5 bg-gray-600 hover:bg-gray-500 rounded-sm text-white">
+              <a className=" p-2 fixed z-10 right-10 bottom-5 bg-[#42414D] hover:bg-gray-600 rounded-sm text-white">
                 <img src={pen} alt="edit" />
               </a>
             </SheetTrigger>
